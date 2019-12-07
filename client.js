e94510f3bfe8419e281a3237ff2f3c5674d896eb
Gonza <gonzaloalcalde96@gmail.com>
2019-12-06 23:20:33 -0300

    Test

--- client.js
+++ client.js
@@ -1,12 +1,19 @@
+//Se establece el servidor al que se conectara el cliente
 var socket = io('http://localhost:3000');
 
+//Se recupera el elemento HTML que contiene el editor de texto
 const editor = document.getElementById("editor");
 
+//Se agrega un evento al editor de texto que se dispara una vez el usuario deja de escribir
 editor.addEventListener("keyup", () => {
+    //Se recupera el texto escrito por el usuario
     const texto = editor.value;
+    //Se envia al servidor dicho texto 
     socket.send(texto);
 })
 
+//Funcion que se va a llamar cuando el cliente reciba un texto por parte del servidor
 socket.on('message', (texto) => {
+    //Se muestra el mensaje recibido en el editor de texto
     editor.value = texto;
 })
--- server.js
+++ server.js
@@ -1,28 +1,40 @@
+//Se crea el servidor http
 const http = require('http').createServer();
+
+//Se crea el socket
 const io = require('socket.io')(http);
+
+//Se establece el numero de puerto
 const port = 3000;
 
+//Se importa la libreria 'fs' para manejo de filesystem
 const fs= require('fs');
 
+//Se pone a escuchar al servidor en el puerto establecido
 http.listen(port, () => console.log(`Servidor escuchando en el puerto: ${port}`));
 
+//Se lee el archivo con el texto editado (si se uso el programa anteriormente)
 let textoCompleto = fs.readFileSync('texto.txt', 'utf8');
-//console.log(textoCompleto);
 
+//Funcion que se va a llamar cuando se conecte un usuario
+//@param: socket del usuario
 io.on('connection', (socket) => {
-    //console.log('Conectado');
-
+    //Se envia al usuario el texto recuperado previamente (en caso de haber usado el programa anteriormente)
     socket.emit('message', textoCompleto);
     
+    //Funcion que se va a llamar cuando se reciba un texto por parte de un usuario
     socket.on('message', (texto) => {
+        //Se guarda el texto
         textoCompleto= texto;
-        //console.log(textoCompleto);
+        //Se reenvia el texto a todos los demas usuarios
         socket.broadcast.emit('message', texto);
     });
 });
 
+//Funcion que se va a llamar cada cierta cantidad de tiempo (10 segundos en este caso)
+//Guarda el ultimo texto recibido en un archivo 'texto.txt'
 setInterval(() => {
     fs.writeFile('texto.txt', textoCompleto, ()=> {
-        //console.log(`Se guardo: ${textoCompleto}`);
+        console.log(`Se guardo: ${textoCompleto}`);
     })
 },10000);
